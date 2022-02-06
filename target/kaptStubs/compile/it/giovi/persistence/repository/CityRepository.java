c601a46471446e50054088d8e9c659bb1f4b21f7
gioxi <desiderio.gio@yahoo.com>
2022-02-06 14:39:43 +0100

    empty git cache

--- target/kaptStubs/compile/it/giovi/persistence/repository/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package it.giovi.persistence.repository;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\u000e\n\u0000\bg\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"Lit/giovi/persistence/repository/CityRepository;", "Lorg/springframework/data/mongodb/repository/MongoRepository;", "Lit/giovi/persistence/entity/CityEntity;", "", "Dashboard-Pollution"})
-@org.springframework.stereotype.Repository()
-public abstract interface CityRepository extends org.springframework.data.mongodb.repository.MongoRepository<it.giovi.persistence.entity.CityEntity, java.lang.String> {
-}
